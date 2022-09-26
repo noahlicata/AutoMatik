6f615b2c1bdbd66c976715bcd55dd6f0c365b19e
Noah Licata <noahinbldr@gmail.com>
2022-09-26 15:31:38 -0600

    added more components

--- client/src/components/CarCard.js
+++ client/src/components/CarCard.js
@@ -13,46 +13,68 @@ function CarCard({ car }) {
   // nonsense
 
   return (
-    <div className="pb-6">
-      <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <Link to={`/cars/${car.id}`}>
-          <img className="rounded-t-lg" src={car.image} alt="" />
-        </Link>
-        <div className="p-5">
-          <Link to={`/cars/${car.id}`}>
-            <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {car.year} {car.make} {car.model}
-            </h5>
-            <h5 className="text-large font-bold tracking-tight text-gray-500 dark:text-white">
-              {car.version}
-            </h5>
-          </Link>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+    <div className="">
+      <Link
+        to={`/cars/${car.id}`}
+        class="flex flex-col items-center bg-white rounded-lg border
+ md:flex-row hover:bg-gray-100 dark:border-gray-700
+      dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <img
+          class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          src={car.image}
+          alt=""
+        />
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {car.year} {car.make} {car.model}
+          </h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
             ${n.toLocaleString()}
           </p>
-          <Link
-            to={`/cars/${car.id}`}
-            href="/cars/:id"
-            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            View Details
-            <svg
-              aria-hidden="true"
-              className="ml-2 -mr-1 w-4 h-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </Link>
         </div>
-      </div>
+      </Link>
     </div>
+    // <div className="pb-6">
+    //   <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    //     <Link to={`/cars/${car.id}`}>
+    //       <img className="rounded-t-lg" src={car.image} alt="" />
+    //     </Link>
+    //     <div className="p-5">
+    //       <Link to={`/cars/${car.id}`}>
+    //         <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+    //           {car.year} {car.make} {car.model}
+    //         </h5>
+    //         <h5 className="text-large font-bold tracking-tight text-gray-500 dark:text-white">
+    //           {car.version}
+    //         </h5>
+    //       </Link>
+    //       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+    //         ${n.toLocaleString()}
+    //       </p>
+    //       <Link
+    //         to={`/cars/${car.id}`}
+    //         href="/cars/:id"
+    //         className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    //       >
+    //         View Details
+    //         <svg
+    //           aria-hidden="true"
+    //           className="ml-2 -mr-1 w-4 h-4"
+    //           fill="currentColor"
+    //           viewBox="0 0 20 20"
+    //           xmlns="http://www.w3.org/2000/svg"
+    //         >
+    //           <path
+    //             fillRule="evenodd"
+    //             d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+    //             clipRule="evenodd"
+    //           ></path>
+    //         </svg>
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -57,7 +57,151 @@ function Shop({ cars }) {
         </div>
       </form>
 
-      <div className="cars">{carsArr}</div>
+      {/* sidebar */}
+
+      <div className="flex justify-center">
+        <aside class="w-64" aria-label="Sidebar">
+          <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
+            <a href="/" class="flex items-center pl-2.5 mb-5">
+              <img
+                src="https://cdns.iconmonstr.com/wp-content/releases/preview/2016/240/iconmonstr-car-3.png"
+                class="mr-3 h-6 sm:h-7"
+                alt="Flowbite Logo"
+              />
+              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                AutoMatik
+              </span>
+            </a>
+            <ul class="space-y-2">
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <span class="ml-3 font-semibold">Your Search</span>
+                </a>
+              </li>
+
+              <label for="underline_select" class="sr-only">
+                Underline select
+              </label>
+              <select
+                id="underline_select"
+                class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+              >
+                <option selected="">Sort By:</option>
+                <option value="US">Price - Lowest</option>
+                <option value="US">Price - Highest</option>
+                <option value="CA">Milage - Lowest</option>
+                <option value="CA">Milage - Highest</option>
+                <option value="FR">Year - Lowest</option>
+                <option value="FR">Year - Highest</option>
+              </select>
+
+              {/* check boxes */}
+
+              <div class="flex items-center mb-4">
+                <input
+                  id="default-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  for="default-checkbox"
+                  class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  New
+                </label>
+              </div>
+              <div class="flex items-center mb-4">
+                <input
+                  id="default-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  for="default-checkbox"
+                  class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Used
+                </label>
+              </div>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    class="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                  </svg>
+                  <span class="flex-1 ml-3 whitespace-nowrap">
+                    Clear Filters
+                  </span>
+                  <span>|</span>
+                  <span class="pl-2">3 Results</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    class="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    ></path>
+                  </svg>
+                  <span class="flex-1 ml-3 whitespace-nowrap">Save Search</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="flex-1 ml-3 whitespace-nowrap">Sign In</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </aside>
+        <div className="flex flex-col">{carsArr}</div>
+      </div>
     </div>
   );
 }
